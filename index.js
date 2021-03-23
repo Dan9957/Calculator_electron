9265819b16d8ee248ca581e6b3a9afa8227ec08b
Dan9957 <danyalghlaby@gmail.com>
2021-03-23 17:29:53 +0430

    cal

---
+++ index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    minWidth: 400,
+    minHeight: 600,
+    maxWidth:400,
+    maxHeight:600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+  win.removeMenu()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
